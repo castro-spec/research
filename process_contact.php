f881d2d0b5123b8fe0bcb2f2c591f53659322254
Macbook <macbook@wiseman.local>
2026-01-30 09:03:04 +0300

    Update landing page and contact form

--- process_contact.php
+++ process_contact.php
@@ -51,7 +51,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             $mail->send();
 
-            echo "Message saved and email sent successfully.";
+        
 
         } catch (Exception $e) {
             echo "Saved in DB but email failed: {$mail->ErrorInfo}";
